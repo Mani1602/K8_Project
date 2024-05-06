34b63e123655bdb46ca9eb984d9a0e80c80735b3
root <root@ip-172-31-6-179.ap-south-1.compute.internal>
2024-05-06 10:01:51 +0000

    webhook test

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 templates = Jinja2Templates(directory="/code")
 
-
 @app.get("/")
 def from_post(request: Request):
     return templates.TemplateResponse('form.html', context={'request': request})
